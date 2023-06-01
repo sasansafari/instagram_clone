f142484abc075625260ddd6b6bd1607881d10044
mmohammadmfallahm02 <mmohammadmfallahm02@gmail.com>
2023-06-01 19:43:52 +0330

    build custom add image screen AppBar widget

--- lib/screens/mainscreen/add_screen.dart
+++ lib/screens/mainscreen/add_screen.dart
@@ -1,22 +1,72 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/src/foundation/key.dart';
-import 'package:flutter/src/widgets/container.dart';
-import 'package:flutter/src/widgets/framework.dart';
 
 class AddScreen extends StatelessWidget {
   const AddScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return SafeArea(
-        child: Scaffold(
-      body: Container(
-        color: Colors.green,
-        child: Center(
-          child: Text("AddScreen",style: TextStyle( fontSize: 28),),
+    return Scaffold(
+      appBar: _buildAppBarWidget(context),
+    );
+  }
+
+  CustomAddImageAppBarWidget _buildAppBarWidget(BuildContext context) {
+    return CustomAddImageAppBarWidget(
+        title: 'Recents',
+        backButton: GestureDetector(
+          onTap: () => Navigator.pop(context),
+          child: const Text('Cancel'),
         ),
+        nextButton: GestureDetector(
+          onTap: () {},
+          child: const Text(
+            'Next',
+            style: TextStyle(color: Colors.blue),
+          ),
+        ),
+        appBarPadding: 14);
+  }
+}
+
+// ******** create custom AppBar ********
+class CustomAddImageAppBarWidget extends StatelessWidget
+    with PreferredSizeWidget {
+  final double appBarHeight;
+  final double appBarPadding;
+  final String title;
+  final Widget? backButton;
+  final Widget? nextButton;
+
+  const CustomAddImageAppBarWidget(
+      {Key? key,
+      this.appBarHeight = 50,
+      required this.title,
+      this.backButton,
+      required this.nextButton,
+      required this.appBarPadding})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+        child: Padding(
+      padding: EdgeInsets.all(appBarPadding),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          backButton ?? const SizedBox(),
+          Align(
+            alignment: Alignment.center,
+            child: Row(
+              children: [Text(title), const Icon(Icons.expand_more)],
+            ),
+          ),
+          nextButton ?? const SizedBox()
+        ],
       ),
     ));
   }
+
+  @override
+  Size get preferredSize => Size.fromHeight(appBarHeight);
 }
