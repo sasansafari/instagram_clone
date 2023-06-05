581988f01deb4d7072803e3968e1a549db370403
Mahdiyar Arbabzi <m.arbabzi@gmail.com>
2023-06-06 04:18:04 +0430

    extract posts widget methods

--- lib/widgets/posts.dart
+++ lib/widgets/posts.dart
@@ -30,56 +30,9 @@ class _PostsState extends State<Posts> {
           height: deviceSize.width,
           child: Stack(
             children: [
-              if (widget.imagesUrl.isNotEmpty)
-                PageView.builder(
-                  itemCount: postsLength,
-                  itemBuilder: (context, index) {
-                    return Image.asset(widget.imagesUrl[index]);
-                  },
-                  onPageChanged: (value) {
-                    setState(() {
-                      _selectedPost = value;
-                    });
-                  },
-                ),
-              if (widget.imagesUrl.isEmpty)
-                Container(
-                  width: deviceSize.width,
-                  height: deviceSize.width,
-                  color: Colors.grey.withOpacity(.4),
-                  child: const Center(
-                    child: Icon(
-                      Icons.hide_image_outlined,
-                      size: 70,
-                      color: Colors.grey,
-                    ),
-                  ),
-                ),
-              if (postsLength > 1)
-                Positioned(
-                  top: 10,
-                  right: 10,
-                  child: Container(
-                    padding: const EdgeInsets.symmetric(
-                      horizontal: 8,
-                      vertical: 3,
-                    ),
-                    height: 26,
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(45),
-                      color: MyColors.postsCounter,
-                    ),
-                    child: Center(
-                      child: Text(
-                        '${_selectedPost + 1}/$postsLength',
-                        style: const TextStyle(
-                          color: Colors.white,
-                          fontSize: 12,
-                        ),
-                      ),
-                    ),
-                  ),
-                ),
+              if (widget.imagesUrl.isNotEmpty) postsSlider(postsLength),
+              if (widget.imagesUrl.isEmpty) emptyPost(deviceSize),
+              if (postsLength > 1) counterPanel(postsLength),
             ],
           ),
         ),
@@ -87,92 +40,154 @@ class _PostsState extends State<Posts> {
           height: 48,
           child: Stack(
             children: [
-              if (postsLength > 1)
-                Positioned(
-                  top: 21,
-                  left: 0,
-                  right: 0,
-                  child: Container(
-                    alignment: Alignment.center,
-                    height: 6,
-                    child: ListView.separated(
-                      separatorBuilder: (context, index) =>
-                          const SizedBox(width: 4),
-                      shrinkWrap: true,
-                      physics: const NeverScrollableScrollPhysics(),
-                      scrollDirection: Axis.horizontal,
-                      itemCount: postsLength,
-                      itemBuilder: (context, index) {
-                        return Container(
-                          width: 6,
-                          height: 6,
-                          decoration: BoxDecoration(
-                            color: index == _selectedPost
-                                ? MyColors.selectedPostPointer
-                                : MyColors.disablePostPointer,
-                            borderRadius: BorderRadius.circular(5),
-                          ),
-                        );
-                      },
-                    ),
-                  ),
-                ),
-              Positioned(
-                top: 13.5,
-                left: 0,
-                right: 0,
-                child: Padding(
-                  padding: const EdgeInsets.symmetric(
-                    horizontal: 14,
-                  ),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      Row(
-                        children: [
-                          CustomIconButton(
-                            onPressed: () {},
-                            icon: Assets.icons.like.svg(),
-                            width: 24,
-                            height: 21,
-                          ),
-                          const SizedBox(
-                            width: 17,
-                          ),
-                          CustomIconButton(
-                            onPressed: () {},
-                            icon: Assets.icons.comment.svg(),
-                            width: 22,
-                            height: 23,
-                          ),
-                          const SizedBox(
-                            width: 17,
-                          ),
-                          CustomIconButton(
-                            onPressed: () {},
-                            icon: Assets.icons.messenger.svg(),
-                            width: 23,
-                            height: 20,
-                          ),
-                          const SizedBox(
-                            width: 17,
-                          ),
-                        ],
-                      ),
-                      CustomIconButton(
-                        onPressed: () {},
-                        icon: Assets.icons.save.svg(),
-                        width: 21,
-                        height: 24,
-                      ),
-                    ],
-                  ),
-                ),
-              ),
+              if (postsLength > 1) counterDots(postsLength),
+              postsPanel(),
             ],
           ),
         ),
       ],
+    );
+  }
+
+  Container emptyPost(Size deviceSize) {
+    return Container(
+      width: deviceSize.width,
+      height: deviceSize.width,
+      color: Colors.grey.withOpacity(.4),
+      child: const Center(
+        child: Icon(
+          Icons.hide_image_outlined,
+          size: 70,
+          color: Colors.grey,
+        ),
+      ),
+    );
+  }
+
+  PageView postsSlider(int postsLength) {
+    return PageView.builder(
+      itemCount: postsLength,
+      itemBuilder: (context, index) {
+        return Image.asset(widget.imagesUrl[index]);
+      },
+      onPageChanged: (value) {
+        setState(() {
+          _selectedPost = value;
+        });
+      },
+    );
+  }
+
+  Positioned counterPanel(int postsLength) {
+    return Positioned(
+      top: 10,
+      right: 10,
+      child: Container(
+        padding: const EdgeInsets.symmetric(
+          horizontal: 8,
+          vertical: 3,
+        ),
+        height: 26,
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(45),
+          color: MyColors.postsCounter,
+        ),
+        child: Center(
+          child: Text(
+            '${_selectedPost + 1}/$postsLength',
+            style: const TextStyle(
+              color: Colors.white,
+              fontSize: 12,
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  Positioned counterDots(int postsLength) {
+    return Positioned(
+      top: 21,
+      left: 0,
+      right: 0,
+      child: Container(
+        alignment: Alignment.center,
+        height: 6,
+        child: ListView.separated(
+          separatorBuilder: (context, index) => const SizedBox(width: 4),
+          shrinkWrap: true,
+          physics: const NeverScrollableScrollPhysics(),
+          scrollDirection: Axis.horizontal,
+          itemCount: postsLength,
+          itemBuilder: (context, index) {
+            return Container(
+              width: 6,
+              height: 6,
+              decoration: BoxDecoration(
+                color: index == _selectedPost
+                    ? MyColors.selectedPostPointer
+                    : MyColors.disablePostPointer,
+                borderRadius: BorderRadius.circular(5),
+              ),
+            );
+          },
+        ),
+      ),
+    );
+  }
+
+  Positioned postsPanel() {
+    return Positioned(
+      top: 13.5,
+      left: 0,
+      right: 0,
+      child: Padding(
+        padding: const EdgeInsets.symmetric(
+          horizontal: 14,
+        ),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Row(
+              children: [
+                CustomIconButton(
+                  onPressed: () {},
+                  icon: Assets.icons.like.svg(),
+                  width: 24,
+                  height: 21,
+                ),
+                const SizedBox(
+                  width: 17,
+                ),
+                CustomIconButton(
+                  onPressed: () {},
+                  icon: Assets.icons.comment.svg(),
+                  width: 22,
+                  height: 23,
+                ),
+                const SizedBox(
+                  width: 17,
+                ),
+                CustomIconButton(
+                  onPressed: () {},
+                  icon: Assets.icons.messenger.svg(),
+                  width: 23,
+                  height: 20,
+                ),
+                const SizedBox(
+                  width: 17,
+                ),
+              ],
+            ),
+            CustomIconButton(
+              onPressed: () {},
+              icon: Assets.icons.save.svg(),
+              width: 21,
+              height: 24,
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
