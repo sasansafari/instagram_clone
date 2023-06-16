2535b910e643f2278d4c1a7cf6684bbbd3fb23fc
paria717 <paria77.gh@gmail.com>
2023-06-16 18:59:47 +0400

    notificationscreen

--- lib/screens/mainscreen/user_profile_screen.dart
+++ lib/screens/mainscreen/user_profile_screen.dart
@@ -1,19 +1,16 @@
 import 'package:flutter/material.dart';
 
-import '../../widgets/special_derawer.dart';
-
 class UserProfileScreen extends StatelessWidget {
   const UserProfileScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return 
-       const SafeArea(
-        child: Scaffold(
-          body: Center(
-            child: Text('UserProfileScreen'),
-          ),
+    return const SafeArea(
+      child: Scaffold(
+        body: Center(
+          child: Text('UserProfileScreen'),
         ),
-      );
+      ),
+    );
   }
 }
