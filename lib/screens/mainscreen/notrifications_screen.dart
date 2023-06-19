2b891904773bc4cd9343f5683902e452d445e387
Mahdiyar Arbabzi <m.arbabzi@gmail.com>
2023-06-19 13:31:27 +0430

    fix a bug on notifications screen

--- lib/screens/mainscreen/notrifications_screen.dart
+++ lib/screens/mainscreen/notrifications_screen.dart
@@ -29,22 +29,26 @@ class _NotificationsScreenState extends State<NotificationsScreen>
       child: Scaffold(
         body: Stack(
           children: [
-            TabBarView(controller: tabController, children: [
-              CustomScrollView(
-                slivers: [
-                  SliverFillRemaining(
+            TabBarView(
+              controller: tabController,
+              children: [
+                CustomScrollView(
+                  slivers: [
+                    SliverFillRemaining(
                       hasScrollBody: false,
-                      child: Column(children: [
-                        58.0.spaceY,
-                        LikeCounterNotification(
+                      child: Column(
+                        children: [
+                          58.0.spaceY,
+                          LikeCounterNotification(
                             imageprofiles: Image.asset(Assets.images.oval.path),
                             imgpostlist: [
                               Image.asset(Assets.images.rectangle1.path),
                               Image.asset(Assets.images.rectangle2.path),
                               Image.asset(Assets.images.rectangle3.path),
                             ],
-                            accountname: accountname[0]),
-                        MultiLikeNotification(
+                            accountname: accountname[0],
+                          ),
+                          MultiLikeNotification(
                             firstimageProfile:
                                 Image.asset(Assets.images.zackjohn.path),
                             secondimageProfile:
@@ -52,13 +56,15 @@ class _NotificationsScreenState extends State<NotificationsScreen>
                             accountname: [accountname[1], accountname[2]],
                             imagePost:
                                 Image.asset(Assets.images.rectangle4.path),
-                            likeCount: 3),
-                        FollowWigetNotification(
+                            likeCount: 3,
+                          ),
+                          FollowWigetNotification(
                             imageprofiles:
                                 Image.asset(Assets.images.kieronD.path),
                             followeraccountname: accountname[1],
-                            accountname: accountname[3]),
-                        LikeCounterNotification(
+                            accountname: accountname[3],
+                          ),
+                          LikeCounterNotification(
                             imageprofiles:
                                 Image.asset(Assets.images.craigLove.path),
                             imgpostlist: [
@@ -71,8 +77,9 @@ class _NotificationsScreenState extends State<NotificationsScreen>
                               Image.asset(Assets.images.rectangle11.path),
                               Image.asset(Assets.images.rectangle13.path),
                             ],
-                            accountname: accountname[3]),
-                        MultiLikeNotification(
+                            accountname: accountname[3],
+                          ),
+                          MultiLikeNotification(
                             firstimageProfile:
                                 Image.asset(Assets.images.zackjohn.path),
                             secondimageProfile:
@@ -80,8 +87,9 @@ class _NotificationsScreenState extends State<NotificationsScreen>
                             accountname: [accountname[1], accountname[2]],
                             imagePost:
                                 Image.asset(Assets.images.rectangle14.path),
-                            likeCount: 3),
-                        MultiLikeNotification(
+                            likeCount: 3,
+                          ),
+                          MultiLikeNotification(
                             firstimageProfile:
                                 Image.asset(Assets.images.zackjohn.path),
                             secondimageProfile:
@@ -89,120 +97,148 @@ class _NotificationsScreenState extends State<NotificationsScreen>
                             accountname: [accountname[1], accountname[2]],
                             imagePost:
                                 Image.asset(Assets.images.rectangle15.path),
-                            likeCount: 3),
-                        CommentWidgetNotification(
+                            likeCount: 3,
+                          ),
+                          CommentWidgetNotification(
                             imgpostlist: Image.asset(Assets.images.oval.path),
                             imageprofiles:
                                 Image.asset(Assets.images.rectangle10.path),
                             followeraccountname: accountname[0],
                             accountname: accountname[1],
-                            commentedUserStory: true)
-                      ])),
-                ],
-              ),
-              CustomScrollView(
-                slivers: [
-                  SliverFillRemaining(
-                    hasScrollBody: false,
-                    child: Expanded(
-                      child: Column(children: [
-                        65.0.spaceY,
-                        SizedBox(
-                          height: size.height / 30,
-                          width: size.width / 1.13,
-                          child: Text(
-                            'Follow Requests',
-                            textAlign: TextAlign.left,
-                            style: MyStyles.getRegularStyle(
+                            commentedUserStory: true,
+                          )
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+                CustomScrollView(
+                  slivers: [
+                    SliverFillRemaining(
+                      hasScrollBody: false,
+                      child: Column(
+                        children: [
+                          65.0.spaceY,
+                          SizedBox(
+                            height: size.height / 30,
+                            width: size.width / 1.13,
+                            child: Text(
+                              'Follow Requests',
+                              textAlign: TextAlign.left,
+                              style: MyStyles.getRegularStyle(
                                 color: MyColors.textnotificationcolor,
-                                fontSize: 15),
+                                fontSize: 15,
+                              ),
+                            ),
                           ),
-                        ),
-                        const Divider(color: MyColors.dividernotificationcolor),
-                        SizedBox(
-                          height: size.height / 26,
-                          width: size.width / 1.13,
-                          child: Text("New",
+                          const Divider(
+                            color: MyColors.dividernotificationcolor,
+                          ),
+                          SizedBox(
+                            height: size.height / 26,
+                            width: size.width / 1.13,
+                            child: Text(
+                              'New',
                               textAlign: TextAlign.left,
                               style: MyStyles.getBoldStyle(
-                                  color: MyColors.textnotificationcolor,
-                                  fontSize: 15)),
-                        ),
-                        LikeCounterNotification(
+                                color: MyColors.textnotificationcolor,
+                                fontSize: 15,
+                              ),
+                            ),
+                          ),
+                          LikeCounterNotification(
                             imgpostlist: [
                               Image.asset(Assets.images.rectangle1.path),
                             ],
                             imageprofiles: imageProfiles[0],
-                            accountname: accountname[0]),
-                        const Divider(color: MyColors.dividernotificationcolor),
-                        SizedBox(
-                          height: size.height / 26,
-                          width: size.width / 1.13,
-                          child: Text('Today',
+                            accountname: accountname[0],
+                          ),
+                          const Divider(
+                            color: MyColors.dividernotificationcolor,
+                          ),
+                          SizedBox(
+                            height: size.height / 26,
+                            width: size.width / 1.13,
+                            child: Text(
+                              'Today',
                               textAlign: TextAlign.left,
                               style: MyStyles.getBoldStyle(
-                                  color: MyColors.textnotificationcolor,
-                                  fontSize: 15)),
-                        ),
-                        MultiLikeNotification(
+                                color: MyColors.textnotificationcolor,
+                                fontSize: 15,
+                              ),
+                            ),
+                          ),
+                          MultiLikeNotification(
                             firstimageProfile: imageProfiles[1],
                             secondimageProfile: imageProfiles[2],
                             accountname: [accountname[1], accountname[2]],
                             imagePost: imagePost,
-                            likeCount: 5),
-                        const Divider(color: MyColors.dividernotificationcolor),
-                        SizedBox(
-                          height: size.height / 26,
-                          width: size.width / 1.13,
-                          child: Text('This Week',
+                            likeCount: 5,
+                          ),
+                          const Divider(
+                            color: MyColors.dividernotificationcolor,
+                          ),
+                          SizedBox(
+                            height: size.height / 26,
+                            width: size.width / 1.13,
+                            child: Text(
+                              'This Week',
                               textAlign: TextAlign.left,
                               style: MyStyles.getBoldStyle(
-                                  color: MyColors.textnotificationcolor,
-                                  fontSize: 15)),
-                        ),
-                        CommentWidgetNotification(
-                          imgpostlist:
-                              Image.asset(Assets.images.craigLove.path),
-                          imageprofiles:
-                              Image.asset(Assets.images.rectangle20.path),
-                          followeraccountname: accountname[3],
-                          accountname: accountname[5],
-                          commentedUserStory: false,
-                        ),
-                        FollowWigetNotification(
+                                color: MyColors.textnotificationcolor,
+                                fontSize: 15,
+                              ),
+                            ),
+                          ),
+                          CommentWidgetNotification(
+                            imgpostlist:
+                                Image.asset(Assets.images.craigLove.path),
+                            imageprofiles:
+                                Image.asset(Assets.images.rectangle20.path),
+                            followeraccountname: accountname[3],
+                            accountname: accountname[5],
+                            commentedUserStory: false,
+                          ),
+                          FollowWigetNotification(
                             imageprofiles:
                                 Image.asset(Assets.images.martiniRound.path),
                             followeraccountname: accountname[4],
-                            accountname: accountname[1]),
-                        FollowWigetNotification(
+                            accountname: accountname[1],
+                          ),
+                          FollowWigetNotification(
                             imageprofiles:
                                 Image.asset(Assets.images.maxjacobson.path),
                             followeraccountname: accountname[5],
-                            accountname: accountname[1]),
-                        FollowWigetNotification(
+                            accountname: accountname[1],
+                          ),
+                          FollowWigetNotification(
                             imageprofiles:
                                 Image.asset(Assets.images.misPotter.path),
                             followeraccountname: accountname[6],
-                            accountname: accountname[1]),
-                        const Divider(color: MyColors.dividernotificationcolor),
-                        SizedBox(
-                          height: size.height / 26,
-                          width: size.width / 1.13,
-                          child: Text(
-                            'This Week',
-                            textAlign: TextAlign.left,
-                            style: MyStyles.getBoldStyle(
-                              color: MyColors.textnotificationcolor,
-                              fontSize: 15,
+                            accountname: accountname[1],
+                          ),
+                          const Divider(
+                            color: MyColors.dividernotificationcolor,
+                          ),
+                          SizedBox(
+                            height: size.height / 26,
+                            width: size.width / 1.13,
+                            child: Text(
+                              'This Week',
+                              textAlign: TextAlign.left,
+                              style: MyStyles.getBoldStyle(
+                                color: MyColors.textnotificationcolor,
+                                fontSize: 15,
+                              ),
                             ),
                           ),
-                        ),
-                      ]),
-                    ),
-                  )
-                ],
-              ),
-            ]),
+                        ],
+                      ),
+                    )
+                  ],
+                ),
+              ],
+            ),
             buildTabBarWidget(tabController),
           ],
         ),
