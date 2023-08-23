d01b2ac524e116c0fd8bfb10ed7fbf5708244726
Silentcry16 <mohammad16m.b@gmail.com>
2023-08-23 14:31:49 +0330

    nothing changed

--- lib/screens/mainscreen/home/home_screen.dart
+++ lib/screens/mainscreen/home/home_screen.dart
@@ -1,6 +1,5 @@
 import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:tec/data/repo/post_repo.dart';
 import 'package:tec/data/src/post_src.dart';
 import 'package:tec/extensions/space_xy_extension.dart';
@@ -28,31 +27,21 @@ class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-
-
     //postRepo.getPosts(0);
 
-
-    return BlocProvider(
-      create: (BuildContext context) {
-
-          return HomeBloc(iPostRepo: iPostRepo, iStoryRepo: iStoryRepo);
-
-        },
-      child: SafeArea(
-        child: Scaffold(
-          body: SingleChildScrollView(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                const MainAppBar(),
-                StoryList(stories: StoriesDatabase.stories),
-                PostsListView(postsData: PostsDatabase.posts),
-                10.0.spaceY,
-                const CircularProgressIndicator(),
-                10.0.spaceY,
-              ],
-            ),
+    return SafeArea(
+      child: Scaffold(
+        body: SingleChildScrollView(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              const MainAppBar(),
+              StoryList(stories: StoriesDatabase.stories),
+              PostsListView(postsData: PostsDatabase.posts),
+              10.0.spaceY,
+              const CircularProgressIndicator(),
+              10.0.spaceY,
+            ],
           ),
         ),
       ),
